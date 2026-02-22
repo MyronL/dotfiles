40d6af9697f337b5aeadf451abbd2499975ec634
Myron Loke <myronloke1@gmail.com>
2026-02-21 18:14:59 -0800

    Update dotfiles

--- nvim/.config/nvim/lua/plugins/eslint.lua
+++ nvim/.config/nvim/lua/plugins/eslint.lua
@@ -4,12 +4,11 @@ return {
     servers = { eslint = {} },
     setup = {
       eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
+        Snacks.util.lsp.on({ name = "eslint" }, function(_, client)
+          client.server_capabilities.documentFormattingProvider = true
+        end)
+        Snacks.util.lsp.on({ name = "vtsls" }, function(_, client)
+          client.server_capabilities.documentFormattingProvider = false
         end)
       end,
     },
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -52,4 +52,9 @@ export PATH="/opt/homebrew/bin:$PATH"
 
 
 
-alias claude="/Users/myronloke/.claude/local/claude"
+# alias claude="/Users/myronloke/.claude/local/claude"
+export PATH="$HOME/.local/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
